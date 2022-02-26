ffe83c946cb840e31209164ab91fa1ac5f0ecaea
vishalksahu <vishal2u_28@yahoo.co.in>
2022-02-26 22:08:09 +0530

    Added new features -- adds two profiles UAT and PROD -- adds Log4j2 instead of system logs -- adds a new rest endpoint for divisble by config value -- adds new configuration for factor

--- src/main/java/com/jashi/learnspring/MathsResource.java
+++ src/main/java/com/jashi/learnspring/MathsResource.java
@@ -1,29 +1,47 @@
 package com.jashi.learnspring;
 
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.Scanner;
+
 @RestController
 @RequestMapping("/calculate")
+@Log4j2
 public class MathsResource {
 
+    @Value( "${mathresource.factor}" )
+    private int factor;
+
+    @Value( "${database.url}" )
+    private String databaseUrl;
+
     @GetMapping(value = "/printvalue")
-    public ResponseEntity printValue (@RequestParam("value") int value) {
-
-        Scanner reader = new Scanner(System.in);
-
+    public ResponseEntity printValue(@RequestParam("value") int value) {
         System.out.print("Enter a value: ");
-
-
         if (value % 2 == 0) {
             System.out.println(value + " is even");
             return new ResponseEntity(value + " is even", HttpStatus.OK);
-        }
-        else {
+        } else {
             System.out.println(value + " is odd");
             return new ResponseEntity(value + " is odd", HttpStatus.OK);
         }
+    }
 
+    @GetMapping(value = "/divisibleByX")
+    public ResponseEntity divisibleByX(@RequestParam("value") int value) {
+        log.info("Database url is {}", databaseUrl);
+        log.info("Received value is {}", value);
+        log.debug("factor from properties file is {}", factor);
+        if (value % factor == 0) {
+          log.debug("divisible by {} is true", factor);
+            return new ResponseEntity(value + " is divisible by "+factor, HttpStatus.OK);
+        }
+        else {
+            log.debug("divisible by {} is false", factor);
+            return new ResponseEntity(value + " is NOT divisible by "+factor, HttpStatus.OK);
+        }
     }
-    }
+}
